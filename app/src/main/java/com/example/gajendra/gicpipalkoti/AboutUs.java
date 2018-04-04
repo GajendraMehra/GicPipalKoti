53e6c9f4ab078df7e2cbd10c05ef4e754d87a813
DESKTOP-N18NO4H\Gajendra <gajendramehra14rgnv@gmail.com>
2018-04-04 07:48:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gajendra/gicpipalkoti/AboutUs.java
@@ -0,0 +1,165 @@
+package com.example.gajendra.gicpipalkoti;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutUs extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    WebView webView;
+    String url="http://gicpipalkoti.000webhostapp.com/aboutus.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        webView = (WebView)findViewById(R.id.webview5);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new AboutUs.MyBrowser());
+        webView.loadUrl(url);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.about_us, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.refresh){
+
+            webView.loadUrl( "javascript:window.location.reload( true )" );
+
+        }
+        if (id == R.id.exit){
+
+            finish();
+            System.exit(0);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.tprofile) {
+            Intent intent= new Intent(AboutUs.this,Teachers.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.sprofile) {
+            Intent intent= new Intent(AboutUs.this,StudentProfile.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.home) {
+            Intent intent= new Intent(AboutUs.this,Home.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.calender) {
+            Intent intent= new Intent(AboutUs.this,calender.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.achievements) {
+            Intent intent= new Intent(AboutUs.this,Achievements.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if (id == R.id.ContactUs) {
+            Intent intent= new Intent(AboutUs.this,ContactUs.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.Downloads) {
+            Intent intent= new Intent(AboutUs.this,Downloads.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/gajendra/gicpipalkoti/exitActivity.java
@@ -0,0 +1,13 @@
+package com.example.gajendra.gicpipalkoti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exit);
+    }
+}
